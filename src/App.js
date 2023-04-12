f8f2b2d983770a0ecf91a88b881d718c1438146e
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-04-11 21:59:08 -0400

    fixed the DOM

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+
+  const [todos,setTodos] = React.useState(
+    
+    [
+
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'fill the squirt gun',
+      isCompleted: false,
+    },
+    {
+      text: 'build app',
+      isCompleted: false,
+    }
+  ]
+  
+  );
+  
+      return (
+        
+        <div className='App-container'>
+  <>
+    {todos.map((todo, i) => <div key={i} className='App-list'>{todo.text}</div>)}
+  </>
     </div>
+  
   );
 }
+
 
 export default App;
